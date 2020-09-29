33b12c9a6646c5a545c8de196fd3dab6a627e065
Michael W Lee <mikewclee@gmail.com>
2020-09-29 12:22:33 -0400

    apps.js components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/portfolio";
+import Contact from "./pages/contact";
+import About from "./pages/about";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+      <Navbar>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+        <Footer />
+      </Navbar>
+      </div>
+    </Router>
   );
 }
 
