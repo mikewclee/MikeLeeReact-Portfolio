0bba2c1e5a743eb6df71ab2b29e0b15ac918a4b4
Michael W Lee <mikewclee@gmail.com>
2020-09-30 15:37:52 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-# React Portfolio
-
-Updated portfolio featuring 6 projects develop with React.
-
-https://mikewclee.github.io/MikeLeeReact-Portfolio/
-
-
---
+++ precache-manifest.785bcb87ce81f7373bb646463bd53d51.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47c3cbaa5e9bdbe467be1886b74c7e79",
+    "url": "/MikeLeeReact-Portfolio/index.html"
+  },
+  {
+    "revision": "f74b173c128a6b4a2f96",
+    "url": "/MikeLeeReact-Portfolio/static/css/main.49a5ab81.chunk.css"
+  },
+  {
+    "revision": "1c6d278d867638b1ce81",
+    "url": "/MikeLeeReact-Portfolio/static/js/2.f95e9cfd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MikeLeeReact-Portfolio/static/js/2.f95e9cfd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74b173c128a6b4a2f96",
+    "url": "/MikeLeeReact-Portfolio/static/js/main.242733fc.chunk.js"
+  },
+  {
+    "revision": "5ea36920cbcc92e261c9",
+    "url": "/MikeLeeReact-Portfolio/static/js/runtime-main.b3cc081e.js"
+  },
+  {
+    "revision": "db28798725a1cf981d11630fa7483fe6",
+    "url": "/MikeLeeReact-Portfolio/static/media/Proj2-peregrine.db287987.JPG"
+  },
+  {
+    "revision": "646d9768b11b1b36fe4deac293525eec",
+    "url": "/MikeLeeReact-Portfolio/static/media/Project1.646d9768.jpg"
+  },
+  {
+    "revision": "bad5a8b30bf12ba650493f80100bcc18",
+    "url": "/MikeLeeReact-Portfolio/static/media/Scheduler.bad5a8b3.jpg"
+  },
+  {
+    "revision": "f9af5c6125d867e28f77fb3b17c8158c",
+    "url": "/MikeLeeReact-Portfolio/static/media/burger.f9af5c61.JPG"
+  },
+  {
+    "revision": "c3a648fad290564c36b7497f07ca11c7",
+    "url": "/MikeLeeReact-Portfolio/static/media/matrix.c3a648fa.jpg"
+  },
+  {
+    "revision": "f6646d97e763d4aed25f035e43e43209",
+    "url": "/MikeLeeReact-Portfolio/static/media/notes.f6646d97.JPG"
+  },
+  {
+    "revision": "5b0c3e30f0028ca2730c066582a193ef",
+    "url": "/MikeLeeReact-Portfolio/static/media/weather.5b0c3e30.jpg"
+  }
+]);
